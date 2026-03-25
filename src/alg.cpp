42c367e0bad9382920b32f2b4114e08c519bc066
pavlushatkachev-max <pavlushatkachev@gmail.com>
2026-03-25 22:44:11 +0300

    alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,17 +1,60 @@
 // Copyright 2025 NNTU-CS
-#include <cstdint>
-#include "alg.h"
-
-uint64_t collatzMaxValue(uint64_t num) {
-  return 1;
-}
-
+#include <stdio.h>
+#include <stdint.h>
 unsigned int collatzLen(uint64_t num) {
-  return 1;
+    unsigned int len = 1;
+    while (num != 1) {
+        if (num & 1) {
+            num = 3 * num + 1;
+        } else {
+            num >>= 1;
+        }
+        len++;
+    }
+    return len;
 }
-
-unsigned int seqCollatz(unsigned int *maxlen,
-                        uint64_t lbound,
-                        uint64_t rbound) {
-  return 1;
+uint64_t collatzMaxValue(uint64_t num) {
+    uint64_t maxVal = num;
+    while (num != 1) {
+        if (num & 1) {
+            num = 3 * num + 1;
+        } else {
+            num >>= 1;
+        }
+        if (num > maxVal) {
+            maxVal = num;
+        }
+    }
+    return maxVal;
+}
+unsigned int seqCollatz(unsigned int *maxlen, uint64_t lbound, uint64_t rbound, uint64_t *maxValue) {
+    unsigned int bestNum = lbound;
+    unsigned int bestLen = collatzLen(lbound);
+    uint64_t bestMaxVal = collatzMaxValue(lbound);
+    for (uint64_t n = lbound + 1; n <= rbound; n++) {
+        unsigned int len = collatzLen(n);
+        if (len > bestLen) {
+            bestLen = len;
+            bestNum = n;
+            bestMaxVal = collatzMaxValue(n);
+        }
+    }
+    if (maxlen) {
+        *maxlen = bestLen;
+    }
+    if (maxValue) {
+        *maxValue = bestMaxVal;
+    }
+    return bestNum;
+}
+int main() {
+    uint64_t lbound = 2;
+    uint64_t rbound = 1000000;
+    unsigned int maxlen;
+    uint64_t maxValue;
+    unsigned int bestNum = seqCollatz(&maxlen, lbound, rbound, &maxValue);
+    printf("Число с самой длинной последовательностью: %u\n", bestNum);
+    printf("Длина последовательности: %u\n", maxlen);
+    printf("Максимальное число в последовательности: %llu\n", maxValue);
+    return 0;
 }
